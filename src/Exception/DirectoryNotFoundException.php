d4a86d5afcf9e18be29b0ff740a3b208ed9379eb
Semen Alekseev <semyon492@ya.ru>
2021-03-02 22:37:14 +0300

    Update

---
+++ src/Exception/DirectoryNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Sura\Cache\Exception;
+
+/**
+ * Class DirectoryNotFoundException
+ * @package Sura\Cache\Exception
+ */
+class DirectoryNotFoundException extends \LogicException
+{
+
+}
--- src/Storages/Journal.php
+++ src/Storages/Journal.php
@@ -12,15 +12,15 @@ interface Journal
 {
     /**
      * Writes entry information into the journal.
+     * @param string $key
+     * @param array $dependencies
      */
-    function write(string $key, array $dependencies): void;
+    public function write(string $key, array $dependencies): void;
 
     /**
      * Cleans entries from journal.
+     * @param array $conditions
      * @return array|null of removed items or null when performing a full cleanup
      */
-    function clean(array $conditions): ?array;
+    public function clean(array $conditions): ?array;
 }
-
-
-class_exists(IJournal::class);
